86694e174490b9665f7a0fb104ca0a11c5ab5101
rishabh <rishabhkumar6592@gmail.com>
2025-02-25 17:44:35 +0530

    done portfolio

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.onload = function() {
     setTimeout(function() {
         document.getElementById('loader').style.display = 'none';
         document.getElementById('content');
-    }, 1800);
+    }, 1200);
 };
 
 
